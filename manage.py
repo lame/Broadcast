77e6001023ee56765085b048c49cbaa00b98a3b8
Ryan Kuhl <rkk09c@gmail.com>
2016-08-11 21:01:01 -0700

    skeleton update to gitignore, consolidating into manage.py

--- manage.py
+++ manage.py
@@ -4,7 +4,9 @@ from flask_migrate import MigrateCommand
 from app import app
 
 manager = Manager(app)
+
 manager.add_command('db', MigrateCommand)
+manager.add_command('run', app.run(debug=True))
 
 if __name__ == '__main__':
     manager.run()
