6c3dc5227a5e96d061ff6c00d1d85d178971bc6f
Ryan Kuhl <rkk09c@gmail.com>
2016-08-11 21:05:47 -0700

    skeleton last update of the night

--- manage.py
+++ manage.py
@@ -6,7 +6,6 @@ from app import app
 manager = Manager(app)
 
 manager.add_command('db', MigrateCommand)
-manager.add_command('run', app.run(debug=True))
 
 if __name__ == '__main__':
     manager.run()
