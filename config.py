c5da11ad1a623696fe06ebfd7ea6c359118c820a
Ryan Kuhl <rkk09c@gmail.com>
2017-01-08 21:02:03 -0800

    Simplified configs

--- config.py
+++ config.py
@@ -6,25 +6,19 @@ class Base(object):
     basedir = os.path.abspath(os.path.dirname(__file__))
     CSRF_ENABLED = True
     DEBUG = False
-    ENV = os.environ['BROADCAST_ENV']
+    ENV = os.environ['ENV']
 
     # SQLAlchemy Config
     DATABASE_QUERY_TIMEOUT = 0.5
     SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
     SQLALCHEMY_RECORD_QUERIES = True
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_DATABASE_URI = '{db_type}://{username}:{password}@{db_url}/{db_name}'.format(
-        db_type='postgresql',
-        username=os.environ['DATABASE_USER_' + ENV],
-        password=os.environ['DATABASE_PASSWORD_' + ENV],
-        db_url=os.environ['DATABASE_URL_' + ENV],
-        db_name=os.environ['DATABASE_NAME_' + ENV]
-    )
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 
     # TWILIO Config
     try:
-        TWILIO_ACCOUNT_SID=os.environ['TWILIO_ACCOUNT_SID_' + ENV]
-        TWILIO_ACCOUNT_AUTH=os.environ['TWILIO_ACCOUNT_AUTH_' + ENV]
+        TWILIO_ACCOUNT_SID=os.environ['TWILIO_ACCOUNT_SID']
+        TWILIO_ACCOUNT_AUTH=os.environ['TWILIO_ACCOUNT_AUTH']
     except KeyError:
         raise('Missing Twilio environ variables')
 
